2cdc425553aea8d5b73bb9d7017439d4343b5a6f
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-11-03 14:20:33 +0200

    simplify dataset path struct

--- src/dataset_path.rs
+++ src/dataset_path.rs
@@ -1,6 +1,4 @@
-use std::path::PathBuf;
-
-use crate::ParticipantCharacteristics;
+use std::{path::PathBuf, convert::AsRef};
 
 #[derive(Debug)]
 pub struct DatasetPath(PathBuf);
@@ -9,14 +7,16 @@ impl DatasetPath {
     pub fn participant_characteristics(&self) -> PathBuf {
         self.0.join("participant_characteristics.csv")
     }
-
-    pub fn participant(&self, chars: &ParticipantCharacteristics) -> PathBuf {
-        self.0.join(chars.participant_id.clone())
-    }
 }
 
 impl From<PathBuf> for DatasetPath {
     fn from(p: PathBuf) -> Self {
         Self(p)
+    }
+}
+
+impl AsRef<PathBuf> for DatasetPath {
+    fn as_ref(&self) -> &PathBuf {
+        &self.0
     }
 }
