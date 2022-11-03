895b8d52526f884d2cf611ca989a56037fc835e4
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-11-03 16:51:52 +0200

    print all webcam videos names example

---
+++ examples/print_webcam_videos_paths.rs
@@ -0,0 +1,37 @@
+use clap::Parser;
+use std::path::PathBuf;
+
+use webgazer_dataset_reader::{DatasetReader, WebcamVideoType};
+
+#[derive(Parser)]
+struct Args {
+    path: PathBuf,
+    video_type: String,
+}
+
+fn main() {
+    let args = Args::parse();
+
+    let video_type: WebcamVideoType = args.video_type.as_str().try_into().unwrap();
+
+    let reader = DatasetReader::new(args.path);
+
+    let readers = reader.participant_readers().unwrap();
+
+    for preader in readers {
+        println!("{}", preader.path.participant_id());
+
+        let video_paths = preader
+            .path
+            .webcam_videos(video_type)
+            .unwrap();
+
+        if video_paths.is_empty() {
+            println!("\tNo videos found.");
+        }
+
+        for video_path in video_paths {
+            println!("\t{}", video_path.file_name().map(|n| n.to_str()).flatten().unwrap());
+        }
+    }
+}
