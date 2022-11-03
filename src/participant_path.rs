567ea68c161b3bcf181d45b77963d9da464783fa
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-11-03 18:34:47 +0200

    refactor webcam video path func

--- src/participant_path.rs
+++ src/participant_path.rs
@@ -1,4 +1,9 @@
-use std::{fs::read_dir, io, path::PathBuf, cmp::Ordering};
+use std::{
+    cmp::Ordering,
+    fs::read_dir,
+    io,
+    path::{Path, PathBuf},
+};
 
 use regex::Regex;
 
@@ -51,74 +56,95 @@ impl ParticipantPath {
             .with_extension("txt")
     }
 
-    pub fn webcam_videos(&self, video_type: Option<WebcamVideoType>, index: Option<usize>) -> io::Result<Vec<PathBuf>> {
-        let regex = Regex::new(&format!(
-            r"{}_{}_-study-{}(\s\(\d+\))?\.webm",
-            self.participant_log_id,
-            index.map_or(r"\d+".to_string(), |i| i.to_string()),
-            video_type.map_or(r"[a-z_]+", |t| t.as_str())
-        ))
-        .unwrap();
-
-        // println!("{:?}", regex);
-
-        let mut paths: Vec<PathBuf> = read_dir(self.root.clone())?
-            .filter_map(|entry| entry.ok())
-            .map(|entry| entry.path())
-            .filter(|path| path.is_file())
-            // .inspect(|path| println!("{:?}", path))
-            .filter(|path| {
-                path.file_name()
-                    .map(|name| name.to_str())
-                    .flatten()
-                    .map_or(false, |name| regex.is_match(name))
-            })
-            .collect();
-
-        let regex_first_index = Regex::new(r"_(\d+)_").unwrap();
-        let regex_aux_index = Regex::new(r"\((\d+)\)").unwrap();
-
-        let parse_indices = |p: &PathBuf| {
-            let name = p.file_name().map(|n| n.to_str()).flatten().unwrap();
-
-            let index: usize = regex_first_index
-                .captures(name)
-                .unwrap()
-                .get(1)
-                .unwrap()
-                .as_str()
-                .parse()
-                .unwrap();
-
-            let aux: Option<usize> = regex_aux_index
-                .captures(name)
-                .map(|caps| caps.get(1))
-                .flatten()
-                .map(|m| m.as_str())
-                .map(|s| usize::from_str_radix(s, 10).ok())
-                .flatten();
-
-            (index, aux)
-        };
-
-        paths.sort_by(|a, b| -> Ordering {
-            let (a_i, a_aux) = parse_indices(a);
-            let (b_i, b_aux) = parse_indices(b);
-
-            match a_i.cmp(&b_i) {
-                Ordering::Equal => {
-                    match (a_aux, b_aux) {
-                        (Some(a_), Some(b_)) => a_.cmp(&b_),
-                        (Some(_), None) => Ordering::Greater,
-                        (None, Some(_)) => Ordering::Less,
-                        (None, None) => Ordering::Equal,
-                    }
-                }
-                Ordering::Greater => Ordering::Greater,
-                Ordering::Less => Ordering::Less,
-            }
-        });
-
-        Ok(paths)
+    pub fn webcam_videos(
+        &self,
+        video_type: Option<WebcamVideoType>,
+        index: Option<usize>,
+    ) -> io::Result<Vec<PathBuf>> {
+        get_webcam_video_paths(&self.root, &self.participant_log_id, video_type, index)
     }
+
+    pub fn webcam_video(
+        &self,
+        video_type: WebcamVideoType,
+        index: usize,
+        n: Option<usize>,
+    ) -> io::Result<Option<PathBuf>> {
+        self.webcam_videos(Some(video_type), Some(index))
+            .map(|paths| paths.iter().nth(n.map_or(0, |n| n)).map(|p| p.to_owned()))
+    }
+}
+
+pub fn get_webcam_video_paths<P: AsRef<Path>>(
+    root: P,
+    participant_log_id: &str,
+    video_type: Option<WebcamVideoType>,
+    index: Option<usize>,
+) -> io::Result<Vec<PathBuf>> {
+    let regex = Regex::new(&format!(
+        r"{}_{}_-study-{}(\s\(\d+\))?\.webm",
+        participant_log_id,
+        index.map_or(r"\d+".to_string(), |i| i.to_string()),
+        video_type.map_or(r"[a-z_]+", |t| t.as_str())
+    ))
+    .unwrap();
+
+    // println!("{:?}", regex);
+
+    let mut paths: Vec<PathBuf> = read_dir(root.as_ref().clone())?
+        .filter_map(|entry| entry.ok())
+        .map(|entry| entry.path())
+        .filter(|path| path.is_file())
+        // .inspect(|path| println!("{:?}", path))
+        .filter(|path| {
+            path.file_name()
+                .map(|name| name.to_str())
+                .flatten()
+                .map_or(false, |name| regex.is_match(name))
+        })
+        .collect();
+
+    let regex_first_index = Regex::new(r"_(\d+)_").unwrap();
+    let regex_aux_index = Regex::new(r"\((\d+)\)").unwrap();
+
+    let parse_indices = |p: &PathBuf| {
+        let name = p.file_name().map(|n| n.to_str()).flatten().unwrap();
+
+        let index: usize = regex_first_index
+            .captures(name)
+            .unwrap()
+            .get(1)
+            .unwrap()
+            .as_str()
+            .parse()
+            .unwrap();
+
+        let aux: Option<usize> = regex_aux_index
+            .captures(name)
+            .map(|caps| caps.get(1))
+            .flatten()
+            .map(|m| m.as_str())
+            .map(|s| usize::from_str_radix(s, 10).ok())
+            .flatten();
+
+        (index, aux)
+    };
+
+    paths.sort_by(|a, b| -> Ordering {
+        let (a_i, a_aux) = parse_indices(a);
+        let (b_i, b_aux) = parse_indices(b);
+
+        match a_i.cmp(&b_i) {
+            Ordering::Equal => match (a_aux, b_aux) {
+                (Some(a_), Some(b_)) => a_.cmp(&b_),
+                (Some(_), None) => Ordering::Greater,
+                (None, Some(_)) => Ordering::Less,
+                (None, None) => Ordering::Equal,
+            },
+            Ordering::Greater => Ordering::Greater,
+            Ordering::Less => Ordering::Less,
+        }
+    });
+
+    Ok(paths)
 }
