5700b12e887effef83331d21691cd353f307d7c4
Rostyslav Bohomaz <rostyslav.db@gmail.com>
2022-11-03 15:44:20 +0200

    refactor participant path and add webcam videos paths

--- src/participant_path.rs
+++ src/participant_path.rs
@@ -1,25 +1,32 @@
-use std::path::PathBuf;
+use std::{fs::read_dir, io, path::PathBuf};
 
-use crate::{DatasetReader, ParticipantCharacteristics};
+use regex::Regex;
+
+use crate::{DatasetReader, ParticipantCharacteristic, ParticipantLogId, WebcamVideoType};
 
 #[derive(Debug)]
 pub struct ParticipantPath {
-    pub participant_id: String,
-    pub participant_log_id: String,
+    pub participant_log_id: ParticipantLogId,
     pub root: PathBuf,
 }
 
 impl ParticipantPath {
-    pub fn new(
-        characteristics: &ParticipantCharacteristics,
+    pub fn from(
+        characteristics: &ParticipantCharacteristic,
         dataset_reader: &DatasetReader,
     ) -> Self {
-        let root = dataset_reader.path.participant(characteristics);
+        let root = dataset_reader
+            .path
+            .as_ref()
+            .join(characteristics.participant_id.clone());
         Self {
-            participant_id: characteristics.participant_id.clone(),
-            participant_log_id: characteristics.participant_log_id.as_millis().to_string(),
+            participant_log_id: characteristics.participant_log_id.clone(),
             root,
         }
+    }
+
+    pub fn participant_id<'a>(&'a self) -> &'a str {
+        self.root.file_name().map(|n| n.to_str()).flatten().unwrap()
     }
 
     pub fn specs(&self) -> PathBuf {
@@ -28,7 +35,7 @@ impl ParticipantPath {
 
     pub fn screen_recording(&self) -> PathBuf {
         self.root
-            .join(self.participant_id.clone())
+            .join(self.participant_id().clone())
             .with_extension("mov")
     }
 
@@ -40,7 +47,32 @@ impl ParticipantPath {
 
     pub fn gaze_predictions(&self) -> PathBuf {
         self.root
-            .join(self.participant_id.clone())
+            .join(self.participant_id().clone())
             .with_extension("txt")
+    }
+
+    pub fn webcam_videos(&self, video_type: WebcamVideoType) -> io::Result<Vec<PathBuf>> {
+        let regex = Regex::new(&format!(
+            r"{}_\d+_-study-{}(\s\(\d+\))?\.webm",
+            self.participant_id(),
+            video_type.as_str()
+        ))
+        .unwrap();
+
+        let mut paths: Vec<PathBuf> = read_dir(self.root.clone())?
+            .filter_map(|entry| entry.ok())
+            .map(|entry| entry.path())
+            .filter(|path| path.is_file())
+            .filter(|path| {
+                path.file_name()
+                    .map(|name| name.to_str())
+                    .flatten()
+                    .map_or(false, |name| regex.is_match(name))
+            })
+            .collect();
+
+        paths.sort();
+
+        Ok(paths)
     }
 }
